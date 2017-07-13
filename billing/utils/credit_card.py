fc731017f15f3ef18cd2d5729ae365bd247d43d3
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-07-13 19:18:25 +0200

    Fixed mastercard regular expression

--- billing/utils/credit_card.py
+++ billing/utils/credit_card.py
@@ -81,7 +81,7 @@ class Visa(CreditCard):
 
 class MasterCard(CreditCard):
     card_name = "MasterCard"
-    regexp = re.compile('^(5[1-5]\d{4}|677189)\d{10}$')
+    regexp = re.compile('^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$')
 
 
 class Discover(CreditCard):
